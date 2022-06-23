bb9629ba7e94554c75e6d1a3590cf94527c4495c
Tony Obriku <tonyobriku@gmail.com>
2022-06-23 22:47:38 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var name = "Tony";
+console.log(name);
+alert(name);
+
+var height = "189cm";
+console.log(height);
+alert(height);
+
+var country = "Nigeria";
+console.log(country);
+alert(country);
